6590df24dba15c4c71a4390a447eb90b2307a472
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-10-09 20:15:44 +0600

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    const belowTwenty = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (num < 20) {
+        return belowTwenty[num];
+    } else if (num < 100) {
+        const ten = Math.floor(num / 10);
+        const remainder = num % 10;
+        return tens[ten] + (remainder > 0 ? " " + belowTwenty[remainder] : "");
+    } else {
+        const hundred = Math.floor(num / 100);
+        const remainder = num % 100;
+        return (
+            belowTwenty[hundred] +
+            " hundred" +
+            (remainder > 0 ? " " + toReadable(remainder) : "")
+        );
+    }
+};
